78792900d86d81f6914c308fb65a679d5341a08a
Aaron Kehl <49955792+AaronKehl@users.noreply.github.com>
2020-07-10 15:12:07 -0400

    Moved PDAL input json files to their own directory and updated paths

--- Mammoth/Scripts/compress.sh
+++ Mammoth/Scripts/compress.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+# Switch to the directory to save the converted to .laz files into
 cd ~/Mammoth/scans
+
+# Activate the PDAL environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pdal
-find /home/mammoth/Mammoth/scans/ -name "*.rxp" | parallel pdal pipeline ~/Mammoth/scripts/base-laz.json --readers.rxp.filename={} --writers.las.filename={.}.laz
 
-find  /home/mammoth/Mammoth/scans/ -name "*.rxp" | parallel gzip {}
+# Clone/convert all .rxp files into .laz using PDAL and its input config json file
+find ~/Mammoth/scans/ -name "*.rxp" | parallel pdal pipeline ~/Mammoth/scripts/PDAL/base-laz.json --readers.rxp.filename={} --writers.las.filename={.}.laz
+
+# Compress all original .rxp files
+find  ~/Mammoth/scans/ -name "*.rxp" | parallel gzip {}
--- Mammoth/Scripts/frame.sh
+++ Mammoth/Scripts/frame.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+
+# Change to the directory where file scans are to be stored to
 cd ~/Mammoth/scans
+
+# Conduct a basic frame scan
 ~/LidarCollect --ip 192.168.0.128 --frame 1 50 130 0.04 110 280 0.04
+
+# Initialize the PDAL environment for scan processing
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate pdal
 
+# Use PDAL to determine the number of points within two different spheres
 filename=$(ls -t *frame.rxp | head -1)
-snowcount=$(pdal pipeline ../scripts/snow-sphere.sh --metadata=stdout --readers.rxp.filename=$filename | jq '.stages["filters.stats"].statistic [].count')
-snowcount2=$(pdal pipeline ../scripts/snow-sphere-2.sh --metadata=stdout --readers.rxp.filename=$filename | jq '.stages["filters.stats"].statistic [].count')
-if [ $snowcount -gt 15 ] && [ $snowcount2 -gt 15 ]
-then
+snowcount=$(pdal pipeline ../scripts/PDAL/snow-sphere.json --metadata=stdout --readers.rxp.filename=$filename | jq '.stages["filters.stats"].statistic [].count')
+snowcount2=$(pdal pipeline ../scripts/PDAL/snow-sphere-2.json --metadata=stdout --readers.rxp.filename=$filename | jq '.stages["filters.stats"].statistic [].count')
+
+# Determine if there are enough points to declare a snowfall event.
+if [ $snowcount -gt 15 ] && [ $snowcount2 -gt 15 ]; then
   echo "Snowing, starting line scan"
   ../scripts/line.sh
 else
